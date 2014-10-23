dfac5c062bf5799a30314259eae38f4cd212a215
egsalinger <egs9108@g.rit.edu>
2014-10-23 09:04:15 -0400

    The map generator will generate some number of rooms (minimum 4) depending on the size of the house. Haven't robustly tested edge cases (very small house, very big house)

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,5 +1,6 @@
 #generator.py
 import sys
+import random
 
 class MapGenerator:
  """This class is the main random floor generator. We will
@@ -10,14 +11,14 @@ class MapGenerator:
  and empty space (.'s).
  A sample house will look like this:
  XXXXXXXXXXXXXXX
- X.............X
- X.............X
- X.............X
- X.............X
- X.............X
- X.............X
- X.............X
- X.............X
+ X.X......X....X
+ X.X......X....X
+ X.X......X....X
+ X.X......X....X
+ XXXXXXXXXXXXXXX
+ X.X...........X
+ X.X...........X
+ X.X...........X
  XXXXXXXXXXXXXXX
 
  2) Given the random house, add additional walls.
@@ -25,31 +26,114 @@ class MapGenerator:
  are connected.
  4) Add at least one entrance/exit.
  5) To add: Windows, hallways, multiple floors."""
+ 
+ wall = 'X'
+ floor = '.'
 
-
- def __init__ (self, width, length):
+ def createFloor (self, width, length):
   self.width = width
   self.length = length
 
   # empty map containing nothing
   x = 0
-  wall = 'X'
-  floor = '.'
   self.map = []
-  for y in range (0, length):
-   temp = ''
-   for x in range (0, width):
+  for x in range (0, width):
+   temp = []
+   for y in range (0, length):
     if (x == 0 or y == 0 or x == width-1 or y == length-1):
-     temp += wall
+     temp.append(self.wall)
     else:
-     temp += floor
+     temp.append (self.floor)
    self.map.append(temp)
+
   if DEBUG:
-   self.print_map()
+   self.printMap()
+
+  # Assume each space = 25 sqft and account for outside.
+  area = (length-2)*(width-2)
+
+  # Assume 100 sqft = average room size
+  # Each space = 25 sqft.
+  # TODO: Walls should be thinner, and placed in between tiles.
+  numWalls = int(area/8)
+  self.generateWalls(numWalls)
+  if DEBUG:
+   self.printMap()
+
+ def generateWalls(self, howMany):
+  i = 0
+  while i < howMany:
+   #TODO: Change room generation to find possible spaces for corners
+   # and then pick random valid choices
+   x = random.randint (0, self.width-1)
+   y = random.randint (0, self.length-1)
+   neighbors = self.getNeighbors (x,y)
+   if self.wall not in neighbors:
+    self.makeWall (x, y)
+   i +=1
+
+
+ def getNeighbors (self, x, y):
+  neighbors = []
+  if x < self.width-1:
+   neighbors.append (self.map [x+1][y])
+  if x > 0:
+   neighbors.append (self.map [x-1][y])
+  if y < self.length -1:
+   neighbors.append (self.map [x][y+1])
+  if y > 0:
+   neighbors.append (self.map [x][y-1])
+
+  return neighbors
+
+
+ def makeWall (self, x, y):
+  curx = x
+  cury = y
+  self.changeToTile (curx, cury, self.wall)
+
+  while curx > 0:
+   curx -=1
+   if self.map [curx][cury] is self.wall:
+    break
+   self.changeToTile (curx, cury, self.wall)
+
+  curx = x
+
+  while curx < self.width -1:
+   curx +=1
+   if self.map [curx][cury] is self.wall:
+    break
+   self.changeToTile (curx, cury, self.wall)
+
+  curx = x
+  
+  while cury > 0:
+   cury -=1
+   if self.map [curx][cury] is self.wall:
+    break
+   self.changeToTile(curx, cury, self.wall)
+  cury = y
+
+  while cury < self.length-1:
+   cury +=1
+   if self.map [curx][cury] is self.wall:
+    break
+   self.changeToTile (curx, cury, self.wall)
+  cury = 1
+
+ def changeToTile (self, curx, cury, tile):
+   self.map [curx][cury] = tile
+   if DEBUG:
+    self.printMap()
+   
 
  def printMap(self):	 
   for row in self.map:
-   print row
+   for col in row:
+    print col,
+   print ''
+  print "\n"
 
 
 
@@ -60,7 +144,7 @@ y = 5
 if len (sys.argv) == 3:
  x = int (sys.argv[1])
  y = int (sys.argv[2])
- if x == 0 or y == 0:
+ if x <= 0 or y <= 0:
   FAILED = True
  if DEBUG:
   print "Running with ", x, ",", y
@@ -72,5 +156,6 @@ if FAILED:
  print "Provided ", len(sys.argv), " args."
  sys.exit (1)
 
-map = MapGenerator(x,y)
+map = MapGenerator()
+map.createFloor (x, y)
 map.printMap()
